3f5d78781506c07bab802e44c8f0232b8ab8f707
Matheus Henrique de Oliveira <82242444+matheushro@users.noreply.github.com>
2023-03-27 21:57:50 -0300

    Update database.sql

--- rest-api/database/database.sql
+++ rest-api/database/database.sql
@@ -3,8 +3,9 @@ use db_tictactoe;
 
 create table users (
 	id int primary key auto_increment,
-    name varchar(50) not null,
-    email varchar(150),
-    nickname varchar(50) not null unique,
-    password varchar(50) not null
+    	name varchar(50) not null,
+    	email varchar(150),
+    	nickname varchar(50) not null unique,
+    	password varchar(50) not null,
+	stats ENUM('ACTIVE', 'DISABLE', 'BLOCK') DEFAULT 'ACTIVE'
 );
